d12e48ce89ae7d3f374a99a1a7b1042c570a5eec
runi12 <166133364+runi12@users.noreply.github.com>
2026-08-15 19:20:27 -0300

    Update and rename if_zika_2D.py to IF_zika_2D.py

--- IF_zika_2D.py
+++ IF_zika_2D.py
@@ -1,7 +1,34 @@
+import os
+import numpy as np
+import zipfile
+import pandas as pd
+
+def fft_padding(img, kernel):
+  p = img.shape[0] + kernel.shape[0] - 1
+  q = img.shape[1] + kernel.shape[1] - 1
+  new_img = np.zeros((p, q))
+  new_img[0:img.shape[0], 0:img.shape[1]]=img
+  new_kernel = np.zeros((p, q))
+  new_kernel[0:kernel.shape[0], 0:kernel.shape[1]]=kernel
+  return new_img, new_kernel
+
+def convolve(img, kernel, iter):
+  new_img, new_kernel = fft_padding(img, kernel)
+  fft_img=np.fft.fft2(new_img)
+  fft_kernel=np.fft.fft2(new_kernel)
+  convolved_img=fft_img * (fft_kernel ** iter)
+  convolved_img=np.round(np.fft.ifft2(convolved_img).real)
+  return convolved_img
+
+def rescale(img):
+  img=img-np.min(img)
+  img=img/np.max(img) * 255
+  return img
+
+
 zip_2d = zipfile.ZipFile('/content/drive/MyDrive/cytation Gustavo/2D stitched.zip')
 imgs_2d=[img for img in zip_2d.namelist() if img.endswith('.tif')]
 column_names = ['picture_id', 'live_cells', 'dead_cells']
-#os.mkdir('/content/2D_stitched')
 df = pd.DataFrame(columns=column_names)
 
 for k in range(int(len(imgs_2d)/2)):
@@ -35,35 +62,26 @@ for k in range(int(len(imgs_2d)/2)):
   y_dead=convolve(img_dead, y_kernel, 1)[5:-5, 5:-5]
   edge_dead = np.sqrt(x_dead**2 + y_dead**2)
   edge_dead = rescale(edge_dead)
-  #img_dead=img_dead.astype('uint8')
-  #dead_values, dead_count = np.unique(img_dead, return_counts=True)
-  #plt.plot(dead_values, dead_count)
-  #plt.show()
 
   ret_dead, thresh_dead = cv2.threshold(edge_dead, 20, 255, cv2.THRESH_BINARY)
-  #thresh_dead=cv2.adaptiveThreshold(img_dead.astype('uint8'), 1, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 25, -5)
   dead_contours, dead_hierarchy = cv2.findContours(thresh_dead.astype('uint8'), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-  blue = np.zeros(img_dead.shape).astype('uint8')[5:-5, 5:-5]
-  merged= cv2.merge([blue, img_live[5:-5, 5:-5], img_dead.astype('uint8')[5:-5, 5:-5]])
   dead_cells_list=[]
   for contour in dead_contours:
     x, y, w, h = cv2.boundingRect(contour)
     if cv2.contourArea(contour)/(w*h) > 0.4:
-      cv2.drawContours(merged, contour, -1, (125,125,255), 1)
       dead_cells_list.append(contour)
-  #[cv2.drawContours(merged, contour, -1, (255,255, 255), 1) for contour in dead_contours if cv2.contourArea(contour)]
+
   live_cells_list=[]
   for contour in live_contours:
     if cv2.contourArea(contour) > 20:
       live_cells_list.append(contour)
-      cv2.drawContours(merged, contour, -1, (255, 125, 125), 1)
-  #cv2.drawContours(merged, live_cells_list, -1, (255, 125, 125), 1)
+      
   column_names = ['picture_id', 'live_cells', 'dead_cells']
   dead_cells=len(dead_cells_list)
   live_cells = len(live_cells_list)
   row_data={'picture_id':name, 'live_cells':live_cells, 'dead_cells':dead_cells}
   row = pd.DataFrame([row_data], columns=column_names)
   df=pd.concat([df, row])
-  merged = cv2.resize(merged, (0, 0), fx = 0.5, fy = 0.5)
-  cv2.imwrite(f'/content/2D_stitched/{name}', merged)
+
+df.to_csv('/content/counts.csv')
